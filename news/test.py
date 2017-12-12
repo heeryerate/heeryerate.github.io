d2618750fb689697f7ff01b4c7c272a1325cea4b
Xi He <xih314@lehigh.edu>
2017-12-12 14:02:42 -0500

    Site updated: 2017-12-12 14:02:39

---
+++ news/test.py
@@ -0,0 +1,18 @@
+############
+
+#   @File name: test.py
+#   @Author:    Xi He
+#   @Email: xih314@lehigh.edu, heeryerate@gmail.com
+
+#   @Create date:   2017-10-02 14:27:51
+
+#   @Last modified by:  Heerye
+#   @Last Modified time:    2017-10-02 14:27:51
+
+#   @Description:
+#   @Example:
+
+#   Don't forget to use control + option + R to re-indent
+
+############
+
